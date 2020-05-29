16bb9cbe2bece4720b39ce8df66e425dffa91f20
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-05-28 23:48:55 -0500

    finished partitioning operations

--- Program.cs
+++ Program.cs
@@ -103,14 +103,102 @@ namespace linq
             IEnumerable<int> ascendingNumbers = from ascendingNumber in numbersList
             orderby ascendingNumber ascending
             select ascendingNumber;
-            
+
             // Display each number that was the acceptable size
             foreach (int ascendingNumber in ascendingNumbers)
             {
                 Console.WriteLine(ascendingNumber);
             }
 
-        }
+            // Output how many numbers are in this list
+            List<int> numbersCount = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
 
+            var numbersCountSum = numbersCount.Count();
+            Console.WriteLine(numbersCountSum);
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+            var totalPurchases = purchases.Sum();
+            Console.WriteLine($"Total sum of purchases = $ {(totalPurchases)}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+            var mostExpensiveProduct = prices.Max();
+            Console.WriteLine($"Our most expensive product is ${mostExpensiveProduct}");
+
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66,
+                12,
+                8,
+                27,
+                82,
+                34,
+                7,
+                50,
+                19,
+                46,
+                81,
+                23,
+                30,
+                4,
+                68,
+                14
+            };
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            
+            var listUntilPerfectSquare = wheresSquaredo.TakeWhile(num =>
+            {
+                return num % (Math.Sqrt(num)) != 0;
+            });
+            foreach (var num in listUntilPerfectSquare) {
+                Console.WriteLine(num);
+            }
+        }
     }
 }
